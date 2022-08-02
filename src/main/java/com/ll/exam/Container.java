d2c1ee391c348d70543ad1e854aa0a7ffc37f054
KCH <doolysmile1@naver.com>
2022-08-02 13:18:18 +0900

    refactor : new 없이 객체 생성

--- src/main/java/com/ll/exam/Container.java
+++ src/main/java/com/ll/exam/Container.java
@@ -2,24 +2,52 @@ package com.ll.exam;
 
 import com.ll.exam.annotation.Controller;
 import com.ll.exam.article.controller.ArticleController;
+import com.ll.exam.home.controller.HomeController;
 import org.reflections.Reflections;
 
+import java.lang.reflect.InvocationTargetException;
 import java.util.ArrayList;
 import java.util.List;
 
 public class Container {
     private static final ArticleController articleController;
+    private static final HomeController homeController;
 
     static {
-        articleController = new ArticleController();
+        try {
+            articleController = ArticleController.class.getDeclaredConstructor().newInstance();
+        } catch (InstantiationException e) {
+            throw new RuntimeException(e);
+        } catch (IllegalAccessException e) {
+            throw new RuntimeException(e);
+        } catch (InvocationTargetException e) {
+            throw new RuntimeException(e);
+        } catch (NoSuchMethodException e) {
+            throw new RuntimeException(e);
+        }
+
+        try {
+            homeController = HomeController.class.getDeclaredConstructor().newInstance();
+        } catch (InstantiationException e) {
+            throw new RuntimeException(e);
+        } catch (IllegalAccessException e) {
+            throw new RuntimeException(e);
+        } catch (InvocationTargetException e) {
+            throw new RuntimeException(e);
+        } catch (NoSuchMethodException e) {
+            throw new RuntimeException(e);
+        }
     }
 
     public static ArticleController getArticleController() {
         return articleController;
     }
 
+    public static HomeController getHomeController() {
+        return homeController;
+    }
 
-    public static List<String> getAllControllerNames() {
+    public static List<String> getControllerNames() {
         List<String> names = new ArrayList<>();
 
         Reflections ref = new Reflections("com.ll.exam");
